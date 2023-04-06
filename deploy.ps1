b4e22c04ebf2783b4cb72f33c37856b4789e6a29
Fadeev Egor <fadeevegor@yandex.ru>
2023-04-06 21:00:16 +0300

    clear all files from http function

--- deploy.ps1
+++
@@ -1,1 +0,0 @@
-gcloud functions deploy http --trigger-http --allow-unauthenticated --runtime python310 --gen2 --region europe-central2 --memory 512MB
--- templates/config_template.md
+++
@@ -1,6 +0,0 @@
-```
-[BOT API]
-URL = https://http-d2ooxt72na-lm.a.run.app/external
-USER ID = $id
-TOKEN = $token
-```
